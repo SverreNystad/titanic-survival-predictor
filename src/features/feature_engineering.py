84a5134de40d42b2835141cd85606ce5e13c9bc5
SverreNystad <sverrenystad@gmail.com>
2024-05-23 01:20:07 +0200

    feat: add feature engineering functions and handle missing values, encode categorical features, and remove unwanted columns

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -2,7 +2,8 @@
 
 import pandas as pd
 import numpy as np
-
+# LabelEncoder
+from sklearn.preprocessing import LabelEncoder
 
 def engineer_features(features: pd.DataFrame) -> pd.DataFrame:
     """
@@ -18,7 +19,30 @@ def engineer_features(features: pd.DataFrame) -> pd.DataFrame:
     features['relatives'] = features['SibSp'] + features['Parch']
     features.loc[features['relatives'] > 0, 'traveled_alone'] = 'No'
     features.loc[features['relatives'] == 0, 'traveled_alone'] = 'Yes'
+
+    features = _handle_missing_values(features)
+    features = _add_family_size_feature(features)
+    features = _process_missing_cabin(features)
+    features = _add_title_feature(features)
+    features = _encode_categorical_features(features)
+    features = _remove_unwanted_columns(features)
+
     return features
+
+def _handle_missing_values(features: pd.DataFrame) -> pd.DataFrame:
+    features['Age'] = features['Age'].fillna(features['Age'].mean())
+    features['Fare'] = features['Fare'].fillna(features['Fare'].mean())
+    features['Embarked'] = features['Embarked'].fillna(features['Embarked'].mode()[0])
+    return features
+
+def _encode_categorical_features(features: pd.DataFrame) -> pd.DataFrame:
+    label_encoders = {}
+    categorical_columns = ['Pclass', 'Sex', 'Ticket', 'Cabin', 'Embarked', 'traveled_alone', "Title", "FamilySize"]
+    for column in categorical_columns:
+        label_encoders[column] = LabelEncoder()
+        features[column] = label_encoders[column].fit_transform(features[column])
+    return features
+
 
 def _remove_unwanted_columns(features: pd.DataFrame) -> pd.DataFrame:
     features = features.drop(["PassengerId","Name","Ticket"], axis=1)
@@ -60,5 +84,4 @@ def _add_title_feature(features: pd.DataFrame) -> pd.DataFrame:
     }
     
     features['Title'] = features['Name'].apply(lambda name: TITLES.get(name.split(',')[1].split('.')[0].strip(), 'Other'))
-    features['Title'] = features['Title'].map(TITLES)
     return features
