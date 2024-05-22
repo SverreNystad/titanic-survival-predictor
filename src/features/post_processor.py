e486544b49575ed9ceef5671e3c2c30115e73b26
SverreNystad <sverrenystad@gmail.com>
2024-05-23 01:22:26 +0200

    feat: Update post-processing function to handle predictions and remove index

--- src/features/post_processor.py
+++ src/features/post_processor.py
@@ -5,4 +5,11 @@ import pandas as pd
 
 def post_process(predictions: pd.DataFrame) -> pd.DataFrame:
     """Post-process the predictions."""
+    predictions["Survived"] = predictions["Survived"].apply(
+        lambda survived: 1 if survived >= 0.5 else 0
+    )
+    predictions["PassengerId"] = predictions.index + 892
+    # Remove index
+    predictions = predictions.set_index("PassengerId")
+
     return predictions
